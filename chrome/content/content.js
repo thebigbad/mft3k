cefed0821c444791d6224f02f68aeb08118e46d6
Ryan Wolf <rwolf@borderstylo.com>
2010-06-25 17:15:59 -0700

    work with frames

--- chrome/content/content.js
+++ chrome/content/content.js
@@ -5,6 +5,10 @@ window.addEventListener(
       "DOMContentLoaded",
       function (event) {
         var doc = event.originalTarget;
+
+        // don't add binding to frames
+        if (doc.defaultView.frameElement) return;
+
         var bindingUrl = 'url("chrome://mft3k/content/binding.xml#seats")';
         doc.body.parentNode.style.MozBinding = bindingUrl;
       },
